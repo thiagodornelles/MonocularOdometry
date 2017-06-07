6f5c1ff9dc5b00e2d6f956e9ae046afccbf90988
Mathias <mathiasfassini@gmail.com>
2017-06-07 17:00:11 -0300

    Implementando o matcher de OpticalFlow e o dataset do KITTI.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <fstream>
+#include <string>
 #include <opencv/cv.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -12,11 +15,77 @@
 using namespace std;
 using namespace cv;
 
+double getAbsoluteScale(int frame_id, char *address){
+
+  string line;
+  int i = 0;
+
+  ifstream myfile (address);
+  double x =0, y=0, z = 0;
+  double x_prev, y_prev, z_prev;
+  if (myfile.is_open())
+  {
+    while (( getline (myfile,line) ) && (i<=frame_id))
+    {
+      z_prev = z;
+      x_prev = x;
+      y_prev = y;
+      std::istringstream in(line);
+      //cout << line << '\n';
+      for (int j=0; j<12; j++)  {
+        in >> z ;
+        if (j==7) y=z;
+        if (j==3)  x=z;
+      }
+
+      i++;
+    }
+    myfile.close();
+  }
+
+  else {
+    cout << "Unable to open file";
+    return 0;
+  }
+
+  return sqrt((x-x_prev)*(x-x_prev) + (y-y_prev)*(y-y_prev) + (z-z_prev)*(z-z_prev)) ;
+
+}
+
+void featureTrackingOpticalFlow(Mat img1, Mat img2, vector<Point2f>& points1, vector<Point2f>& points2, vector<uchar>& status){
+    vector<float> err;
+
+    Size winSize=Size(21,21);
+    TermCriteria termcrit = TermCriteria(TermCriteria::COUNT+TermCriteria::EPS,30,0.01);
+
+    calcOpticalFlowPyrLK(img1, img2, points1, points2, status, err, winSize, 3, termcrit, 0, 0.001);
+
+    int indexCorrection = 0;
+
+    for(int i = 0; i < status.size(); i++){
+        Point2f pt = points2.at(i - indexCorrection);
+        if((status.at(i) == 0) || (pt.x < 0) || (pt.y < 0)){
+            if((pt.x < 0) || (pt.y < 0))
+                status.at(i) = 0;
+            points1.erase(points1.begin() + (i - indexCorrection));
+            points2.erase(points2.begin() + (i - indexCorrection));
+            indexCorrection++;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
+   /*-------COMANDOS----------
+    * argv[1] - endereco do video
+    * argv[2] - endereco do ground-truth
+   */
 
     Ptr<Feature2D> orb = ORB::create(200);
     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
-    VideoCapture cap("../../../video2.avi");
+
+    VideoCapture cap(argv[1]);
+
+
 //    481.20,	0,          319.50
 //    0,        -480.00,	239.50
 //    0,        0,          1
@@ -32,10 +101,11 @@ int main(int argc, char *argv[]) {
     Mat E;
     Point2d p0 = Point2d(250, 250);
     cap >> frame1;
+    int num_frame = 1;
     while(cap.isOpened()){
-        for (int i = 0; i < 10 && cap.isOpened(); ++i) {
+//        for (int i = 0; i < 10 && cap.isOpened(); ++i) {
             cap >> frame2;
-        }
+//        }
         if(frame2.empty()) break;
 
         orb->detectAndCompute(frame1, noArray(), kps1, desc1, false);
@@ -70,14 +140,19 @@ int main(int argc, char *argv[]) {
         }
 //        drawMatches(frame1, kps1, frame2, kps2, good, output);
 //        Point2d pp = Point2d(319.50f, 239.50f);
-        Point2d pp = Point2d(0, 0);
-        E = findEssentialMat(points1, points2, 1.f/-480.f, pp, RANSAC, 0.9999999);
+//        Point2d pp = Point2d(0, 0);
+        Point2d pp = Point2d(607.1928, 185.2157);
+        float focal = 718.8560;
+        E = findEssentialMat(points1, points2, /*1.f/-480.f*/focal, pp, RANSAC, 0.9999999);
         Mat t_f = Mat::zeros(3,1, CV_64F);
         Mat R_f;
-        recoverPose(E, points1, points2, R_f, t_f);
-        cout << R << endl;
-        cout << t << endl;
-        t = t + 8.f*(R_f*t_f);
+        recoverPose(E, points1, points2, R_f, t_f, focal, pp);
+        float scale = getAbsoluteScale(num_frame++, argv[2]);
+
+//        cout << R << endl;
+//        cout << t << endl;
+        cout <<"Num frame:"<<num_frame<<" - Scale:"<< scale << endl;
+        t = t + scale*(R_f*t_f);
         R = R_f*R;
 
 //        traj.setTo(0);
